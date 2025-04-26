b227e375850f086c675aee638658b763b5d3c948
patikpe <patrickpdev@pm.me>
2025-04-26 19:15:45 +0200

    Refactor TekioForm to StatefulWidget for dynamic section navigation; update TekioSection to include section key for improved state management.

--- lib/widgets/tekio_form_builder.dart
+++ lib/widgets/tekio_form_builder.dart
@@ -3,49 +3,85 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:tekio_forms/models/models.dart';
 import 'package:tekio_forms/widgets/tekio_form.dart';
 
-class TekioForm extends StatelessWidget {
-  TekioForm({super.key, required this.formData});
+class TekioForm extends StatefulWidget {
+  const TekioForm({super.key, required this.formData});
   final TekioFormData formData;
+
+  @override
+  State<TekioForm> createState() => _TekioFormState();
+}
+
+class _TekioFormState extends State<TekioForm> {
   final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
+  int indexToLoad = 0; //TODO:THIS IS STUPID ONELY HERE FOR POC REASONS
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(formData.formTile ?? ''),
+        title: Text(widget.formData.formTile ?? ''),
         centerTitle: true,
-        // bottom: PreferredSize(
-        //   preferredSize: Size.fromHeight(50.0),
-        //   child: Row(
-        //     children: [
-        //       IconButton(
-        //           onPressed: () {}, icon: Icon(Icons.arrow_back_rounded)),
-        //       Expanded(
-        //           child: Text(
-        //         "TEXT DAwdwTA",
-        //         textAlign: TextAlign.center,
-        //       )),
-        //       IconButton(
-        //           onPressed: () {}, icon: Icon(Icons.arrow_forward_rounded))
-        //     ],
-        //   ),
-        // ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    //TODO:THIS IS STUPID ONELY HERE FOR POC REASONS
+                    if (indexToLoad > 0) {
+                      setState(() {
+                        indexToLoad--;
+                      });
+                    }
+                  },
+                  icon: Icon(Icons.arrow_back_rounded)),
+              Expanded(
+                  child: Text(
+                widget.formData.formSections[indexToLoad].sectionTitle ?? '',
+                textAlign: TextAlign.center,
+              )),
+              IconButton(
+                onPressed: () {
+                  //TODO:THIS IS STUPID ONELY HERE FOR POC REASONS
+                  if (indexToLoad < widget.formData.formSections.length - 1) {
+                    setState(() {
+                      indexToLoad++;
+                    });
+                  }
+                },
+                icon: Icon(Icons.arrow_forward_rounded),
+              )
+            ],
+          ),
+        ),
       ),
       body: FormBuilder(
         key: _formKey,
         child: TekioSection(
-          formSectionData: formData.formSections.first,
+          formSectionData: widget.formData.formSections[indexToLoad],
         ),
       ),
       bottomNavigationBar: SizedBox(
         width: double.infinity,
-        child: FilledButton(
-          onPressed: () {
-            _formKey.currentState?.saveAndValidate();
-            debugPrint(_formKey.currentState?.value.toString());
-          },
-          child: Text('DAT BUTTON'),
-        ),
+        child: //TODO:THIS IS STUPID ONELY HERE FOR POC REASONS
+            indexToLoad == widget.formData.formSections.length - 1
+                ? FilledButton(
+                    onPressed: () {
+                      _formKey.currentState?.saveAndValidate();
+                      debugPrint(_formKey.currentState?.value.toString());
+                    },
+                    child: Text('YES DO THE THING'),
+                  )
+                : FilledButton(
+                    onPressed: () {
+                      setState(() {
+                        indexToLoad++;
+                      });
+                      ;
+                    },
+                    child: Text('DAS BUTTON'),
+                  ),
       ),
     );
   }
--- lib/widgets/tekio_section.dart
+++ lib/widgets/tekio_section.dart
@@ -3,8 +3,9 @@ import 'package:tekio_forms/models/models.dart';
 import 'package:tekio_forms/widgets/tekio_form.dart';
 
 class TekioSection extends ListView {
-  TekioSection({super.key, required TekioFormSection formSectionData})
+  TekioSection({required TekioFormSection formSectionData})
       : super(
+          key: Key(formSectionData.sectionKey),
           scrollDirection: Axis.vertical,
           children: formSectionData.formFields!.map(
             (e) {
