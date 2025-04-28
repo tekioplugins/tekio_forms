0727fe89f8549ff47dc3fbea31cc1863b4862cf3
patikpe <patrickpdev@pm.me>
2025-04-28 11:15:23 +0200

    Refactor TekioSection to improve title and subtitle rendering logic; update JSON to set null values for empty titles and subtitles.

--- lib/widgets/tekio_section.dart
+++ lib/widgets/tekio_section.dart
@@ -9,36 +9,44 @@ class TekioSection extends Column {
   }) : super(
           key: Key(formSectionData.sectionKey),
           spacing: 10.0,
-          children: List.from([
-            ListTile(
-              title: (formSectionData.sectionTitle != null &&
-                      formSectionData.sectionTitle!.isNotEmpty)
-                  ? Text(
-                      formSectionData.sectionTitle ?? '',
-                      textAlign: TextAlign.center,
-                    )
-                  : null,
-              subtitle: (formSectionData.sectionSubtitle != null &&
-                      formSectionData.sectionSubtitle!.isNotEmpty)
-                  ? Text(
-                      formSectionData.sectionSubtitle ?? '',
-                      textAlign: TextAlign.center,
-                    )
-                  : null,
-            ),
-          ])
-            ..addAll(
-              formSectionData.formFields?.map((e) {
-                    switch (e.fieldType) {
-                      case TekioFieldType.textField:
-                        return TekioTextField(
-                          formFieldsData: e,
-                        );
-                      default:
-                        return SizedBox();
-                    }
-                  }).toList() ??
-                  [],
+          children: List.from(
+            formSectionData.formFields?.map((e) {
+                  switch (e.fieldType) {
+                    case TekioFieldType.textField:
+                      return TekioTextField(
+                        formFieldsData: e,
+                      );
+                    default:
+                      return SizedBox();
+                  }
+                }).toList() ??
+                [],
+          )..insertAll(
+              0,
+              (formSectionData.sectionTitle != null ||
+                      formSectionData.sectionSubtitle != null)
+                  ? [
+                      ListTile(
+                        title: (formSectionData.sectionTitle != null)
+                            ? Text(
+                                formSectionData.sectionTitle ?? '',
+                                textAlign: TextAlign.center,
+                              )
+                            : Text(
+                                formSectionData.sectionSubtitle ?? '',
+                                textAlign: TextAlign.center,
+                              ),
+                        subtitle: (formSectionData.sectionSubtitle != null)
+                            ? !(formSectionData.sectionTitle == null)
+                                ? Text(
+                                    formSectionData.sectionSubtitle ?? '',
+                                    textAlign: TextAlign.center,
+                                  )
+                                : null
+                            : null,
+                      ),
+                    ]
+                  : [],
             ),
         );
 }
