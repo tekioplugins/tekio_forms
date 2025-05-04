b575b60867fd86da336866073170d89a1b0cd831
patikpe <patrickpdev@pm.me>
2025-05-04 16:18:17 +0200

    Enhance documentation in TekioFormDecoration by adding comments for formPadding, sectionPadding, buttonPadding, and formsSpacing properties.

--- lib/models/tekio_form_decoration.dart
+++ lib/models/tekio_form_decoration.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 class TekioFormDecoration {
+  /// Padding for the base form
   final EdgeInsetsGeometry formPadding;
+
+  /// Padding for the sections
   final EdgeInsetsGeometry sectionPadding;
+
+  /// Submit button padding
   final EdgeInsetsGeometry buttonPadding;
+
+  /// Spacing between the form fields
   final double formsSpacing;
 
   TekioFormDecoration({
