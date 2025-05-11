78a4fa92abdc7b3f80a926f04ee6e0f2d8ee9165
patikpe <patrickpdev@pm.me>
2025-05-11 11:20:26 +0200

    Refactor TekioFormTitle test by removing unused imports and simplifying formData initialization

--- test/widgets/tekio_form_title_test.dart
+++ test/widgets/tekio_form_title_test.dart
@@ -1,34 +1,27 @@
-// import 'dart:convert';
-
 // import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
 // import 'package:flutter_test/flutter_test.dart';
 // import 'package:tekio_forms/models/tekio_form_data.dart';
 // import 'package:tekio_forms/widgets/tekio_form_title.dart';
+// import '../test_util_data/test_utils/test_data.dart';
+// import '../test_util_data/test_utils/test_init.dart';
 
 // Future<void> main() async {
-//   TestWidgetsFlutterBinding.ensureInitialized();
-
-//   final String jsonString = await rootBundle.loadString(
-//       'test/test_json_data/test_form_build_data/json_form_data_01.json');
-
-//   final TekioFormData formData = TekioFormData.fromJson(jsonDecode(jsonString));
+//   TestInit.initBasics();
+//   final TekioFormData formData = await TestData.basicFormData01;
 //   testWidgets('TekioFormTitle widget is build', (tester) async {
 //     await tester.pumpWidget(
 //       Material(
 //         child: Scaffold(
 //           appBar: AppBar(
 //             title: TekioFormTitle(
-//               formData: formData,
+//               title: formData.formTile,
+//               subtitle: formData.formSubTitle,
 //             ),
 //           ),
 //         ),
 //       ),
 //     );
-
-//     // await tester.pump();
 //     expect(find.text('form_title'), findsOneWidget);
-
-//     // expect(find.byType(TekioFormTitle), findsOneWidget);
+//     expect(find.byType(TekioFormTitle), findsOneWidget);
 //   });
 // }
