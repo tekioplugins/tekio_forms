985f43eab78674545d08762fb314760a17e6404b
patikpe <patrickpdev@pm.me>
2025-05-11 20:43:35 +0200

    Comment out the main test function and related imports in text_field_test.dart

--- test/widgets/form_fields/text_field_test.dart
+++ test/widgets/form_fields/text_field_test.dart
@@ -1,1 +1,10 @@
-void main() {}
+// import 'package:flutter_test/flutter_test.dart';
+
+// void main() {
+//   testWidgets(
+//     "Testing Text Form Field",
+//     (widgetTester) {
+
+//     },
+//   );
+// }
