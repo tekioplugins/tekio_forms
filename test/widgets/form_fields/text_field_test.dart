76af629b71e22ee0de484c638e31d9fa2dbefaba
patikpe <patrickpdev@pm.me>
2025-05-14 15:00:31 +0200

    Refactor test files by uncommenting and organizing imports; restore main test functions for TekioTextField and TekioFormTitle

--- test/widgets/form_fields/text_field_test.dart
+++ test/widgets/form_fields/text_field_test.dart
@@ -1,10 +1,28 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tekio_forms/models/models.dart';
+import 'package:tekio_forms/widgets/tekio_form.dart';
 
-// void main() {
-//   testWidgets(
-//     "Testing Text Form Field",
-//     (widgetTester) {
+Future<void> main() async {
+  testWidgets(
+    "Testing Text Form Field",
+    (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: TekioTextField(
+              formFieldsData: TekioFieldData(
+                fieldKey: 'test_key',
+                fieldType: TekioFieldType.textField,
+                fieldLabel: 'test_text_label',
+              ),
+            ),
+          ),
+        ),
+      );
 
-//     },
-//   );
-// }
+      expect(find.byKey(Key('test_key')), findsOneWidget);
+      expect(find.byType(TekioTextField), findsOneWidget);
+    },
+  );
+}
--- test/widgets/tekio_form_title_test.dart
+++ test/widgets/tekio_form_title_test.dart
@@ -1,27 +1,30 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:tekio_forms/models/tekio_form_data.dart';
-// import 'package:tekio_forms/widgets/tekio_form_title.dart';
-// import '../test_util_data/test_utils/test_data.dart';
-// import '../test_util_data/test_utils/test_init.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tekio_forms/models/tekio_form_data.dart';
+import 'package:tekio_forms/widgets/tekio_form_title.dart';
+import '../test_util_data/test_utils/test_data.dart';
+import '../test_util_data/test_utils/test_init.dart';
 
-// Future<void> main() async {
-//   TestInit.initBasics();
-//   final TekioFormData formData = await TestData.basicFormData01;
-//   testWidgets('TekioFormTitle widget is build', (tester) async {
-//     await tester.pumpWidget(
-//       Material(
-//         child: Scaffold(
-//           appBar: AppBar(
-//             title: TekioFormTitle(
-//               title: formData.formTile,
-//               subtitle: formData.formSubTitle,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//     expect(find.text('form_title'), findsOneWidget);
-//     expect(find.byType(TekioFormTitle), findsOneWidget);
-//   });
-// }
+Future<void> main() async {
+  TestInit.initBasics();
+  final TekioFormData formData = await TestData.basicFormData01;
+  testWidgets(
+    'TekioFormTitle widget is build',
+    (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: TekioFormTitle(
+                title: formData.formTile,
+                subtitle: formData.formSubTitle,
+              ),
+            ),
+          ),
+        ),
+      );
+      expect(find.text('form_title'), findsOneWidget);
+      expect(find.byType(TekioFormTitle), findsOneWidget);
+    },
+  );
+}
